a7ffda1134993f7d2025388c54067f8986477a0e
Diego Cerda <diego.holland@outlook.com>
2018-10-14 22:17:49 -0500

    lm,xl,c

--- src/main/java/servlet/HelloServlet.java
+++ src/main/java/servlet/HelloServlet.java
@@ -1,26 +1,20 @@
 package servlet;
 
 import java.io.IOException;
-import java.io.InputStream;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
- 
+
 import javax.servlet.ServletException;
-import javax.servlet.annotation.MultipartConfig;
+import javax.servlet.ServletOutputStream;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.Part;
 
 @WebServlet(
         name = "MyServlet", 
         urlPatterns = {"/hello"}
     )
 public class HelloServlet extends HttpServlet {
-
+int sudnd;
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
@@ -28,6 +22,7 @@ public class HelloServlet extends HttpServlet {
         out.write("hello heroku".getBytes());
         out.flush();
         out.close();
+
     }
     
 }
