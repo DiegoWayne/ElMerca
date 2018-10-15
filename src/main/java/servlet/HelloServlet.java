37f43c4a94e07f3b4eb6d51c422f21d5f8e43788
Diego Cerda <diego.holland@outlook.com>
2018-10-14 22:12:00 -0500

    uchudhfudhvu

--- src/main/java/servlet/HelloServlet.java
+++ src/main/java/servlet/HelloServlet.java
@@ -1,19 +1,13 @@
 package servlet;
 
 import java.io.IOException;
-import java.io.InputStream;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
- 
+
 import javax.servlet.ServletException;
-import javax.servlet.annotation.MultipartConfig;
+import javax.servlet.ServletOutputStream;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.Part;
 
 @WebServlet(
         name = "MyServlet", 
@@ -21,10 +15,7 @@ import javax.servlet.http.Part;
     )
 public class HelloServlet extends HttpServlet {
 
-    private String dbURL = "jdbc:mysql://us-cdbr-iron-east-01.cleardb.net/heroku_38a1979085a7b59";
-    private String dbUser = "becdff0c984df4";
-    private String dbPass = "51e4aab00b5ef5b";
-     
+    @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
         ServletOutputStream out = resp.getOutputStream();
@@ -32,5 +23,5 @@ public class HelloServlet extends HttpServlet {
         out.flush();
         out.close();
     }
-
+    
 }
